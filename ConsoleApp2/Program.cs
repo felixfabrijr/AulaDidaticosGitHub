77c69eae82e3587e6b50eec54c4dc10a9999c46c
felixfabri <felix.fabrijr@gmail.com>
2023-01-30 08:28:34 +0000

    aula teste

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("")
+            Console.WriteLine("Aula teste GitHub");
         }
     }
 }
